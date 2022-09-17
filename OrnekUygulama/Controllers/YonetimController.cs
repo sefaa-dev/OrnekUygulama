8159c93481b3c6f7668535e250387a1d250b3986
Sefa Yılmaz <sefayilmaz582@gmail.com>
2022-09-18 01:12:16 +0300

    Admin panel giydirme

---
+++ OrnekUygulama/Controllers/YonetimController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrnekUygulama.Controllers
+{
+    public class YonetimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Tarifler()
+        {
+            return View();
+        }
+        public IActionResult Yorumlar()
+        {
+            return View();
+        }
+        public IActionResult Kullanicilar()
+        {
+            return View();
+        }
+        public IActionResult Bilgilerim()
+        {
+            return View();
+        }
+        public IActionResult CikisYap()
+        {
+            return View();
+        }
+    }
+}
